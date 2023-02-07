0d1dd333d03387d33f1cc0e1ef55e53efd9c8f82
ccardo <riccardo.gallo.pb+github@gmail.com>
2023-02-08 00:45:06 +0100

    yea

---
+++ src/bin/when_turn_100.rs
@@ -0,0 +1,26 @@
+use std::{io::{self, Write}, fmt::Error};
+use chrono::{self, Utc, Datelike};
+
+fn main() {
+    let name = input("What is you name? ").expect("Failed to read line");
+    println!("Hello, {}!", name);
+
+    let age = input("How old are you? ")
+        .expect("Germanio!")
+        .trim().parse::<i32>()
+        .expect("Failed to convert into integer.");
+
+    let current_year = Utc::now().year();
+    let year_of_100 = current_year + 100 - age;
+    println!("You will turn 100 in {}", year_of_100)
+}
+
+fn input(message: &str) -> io::Result<String> {
+    print!("{}", message);
+    io::stdout().flush()?;
+
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input)?;
+
+    Ok(user_input.trim().to_owned())
+}
