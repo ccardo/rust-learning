d6077f71c7044e4e64b8e0fe75f8a80921e6f2b9
ccardo <riccardo.gallo.pb+github@gmail.com>
2023-02-16 20:24:36 +0100

    yed

--- src/bin/poker.rs
+++ src/bin/poker.rs
@@ -1,85 +1,80 @@
-use rand::{seq::SliceRandom, thread_rng};
 use itertools::{self, Itertools};
+use rand::{seq::SliceRandom, thread_rng};
 const SUITS: [&str; 4] = ["Hearts", "Diamonds", "Clubs", "Spades"];
 
 fn main() {
-  let mut my_deck = create_deck();
-  shuffle_deck(&mut my_deck);
+    let mut my_deck = create_deck();
+    shuffle_deck(&mut my_deck);
 
-  let mut hands: Vec<Vec<&Card>> = vec![];
-  let mut hand: Vec<&Card> = vec![];
-  for (i, card) in my_deck.iter().enumerate() {
-    hand.push(card);
+    let mut hands: Vec<Vec<&Card>> = vec![];
+    let mut hand: Vec<&Card> = vec![];
+    for (i, card) in my_deck.iter().enumerate() {
+        hand.push(card);
 
-    let index: i32 = i.try_into().unwrap();
-    if index % 5 == 0 {
-      hands.push(hand.clone());
-      hand.clear()
+        let index: i32 = i.try_into().unwrap();
+        if (index + 1) % 5 == 0 {
+            hands.push(hand.clone());
+            hand.clear()
+        }
     }
-  }
 
-  let first_hand = &hands[0];
-  /* let result = check_combinations(first_hand.to_vec()).unwrap();
-  println!("{}", result) */
-  let first_iter_hand = first_hand.into_iter();
-  let values = first_iter_hand.map(|x| x.value);
+    let first_hand = &hands[0];
+    let first_iter_hand = first_hand.into_iter();
+    let values = first_iter_hand.map(|x| x.value);
 
-  for value in values.clone().collect::<Vec<i32>>() {
-    println!("{}", value)
-  }
+    let mut binding = values.collect_vec();
+    binding.sort_by(|&a, b| a.cmp(b));
 
-  let mut binding = values.collect_vec();
-  binding.sort_by(|&a, b| a.cmp(b));
-
-  for value in &binding {
-    println!("{}", value)
-  }
-
-  let result = coppia(&binding).expect("No coppia found.");
-  println!("{}", result)
+    let result = match duplicati(&mut binding) {
+        Some(coppia) => coppia,
+        None => ""
+    };
+    println!("{}", result)
 }
 
 struct Card {
-  suit: String,
-  value: i32
+    suit: String,
+    value: i32,
 }
 
 fn create_deck() -> Vec<Card> {
-  let mut deck: Vec<Card> = vec![];
+    let mut deck: Vec<Card> = vec![];
 
-  for suit in SUITS {
-    for value in 7..=14 {
-      let card: Card = Card {
-        suit: suit.to_owned(), 
-        value};
-      deck.push(card)
+    for suit in SUITS {
+        for value in 7..=14 {
+            let card: Card = Card {
+                suit: suit.to_owned(),
+                value,
+            };
+            deck.push(card)
+        }
     }
-  };
-  deck
+    deck
 }
 
 fn shuffle_deck(deck: &mut Vec<Card>) -> () {
-  deck.shuffle(&mut thread_rng())
+    deck.shuffle(&mut thread_rng())
 }
 
-fn check_combinations(hand: Vec<&Card>) -> Option<&str> {
+fn duplicati(values: &mut Vec<i32>) -> Option<&str> {
+    let duplicates = values.into_iter().dedup_with_count().collect_vec();
 
-  let iter_hand = hand.into_iter();
-  let values = iter_hand.map(|x| x.value);
+    for i in 2..=4 {
+        let boolean = if (&duplicates).into_iter()
+            .any(|x| x.0 == i) { true } else {false};
 
-  // duplicate values
-  let duplicates = values.dedup_with_count();
-  if duplicates.clone().count() == 2 {
-
-    Some("Full")
-  } else if duplicates.clone().count() == 1 {
-    Some("Coppia")
-  } else { None }
-  // INCORRECT CODE (FIX)
-}
-
-fn coppia(hand: &Vec<i32>) -> Option<&str> {
-
-  // do somtething...
-  Some("working!")
+        if boolean {
+            return match i {
+                4 => Some("Poker"),
+                3 => Some("Tris"),
+                2 => {return match duplicates.len() {
+                    4 => Some("Coppia"),
+                    3 => Some("Doppia Coppia"),
+                    2 => Some("Full Hand"),
+                    _ => None};}
+                _ => None
+            };
+        }
+    }
+    None
 }
