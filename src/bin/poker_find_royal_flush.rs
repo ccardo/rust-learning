8cc6f33882121665dafd5614c9c384c07957b8b8
ccardo <riccardo.gallo.pb+github@gmail.com>
2023-02-19 09:28:00 +0100

    fmt

--- src/bin/poker_find_royal_flush.rs
+++ src/bin/poker_find_royal_flush.rs
@@ -1,32 +1,30 @@
 mod poker;
-use poker::{Card, create_deck, royal_flush, shuffle_deck};
+use poker::{create_deck, royal_flush, shuffle_deck, Card};
 
 fn main() {
+    let mut n: u32 = 1;
+    'search: loop {
+        n += 1;
+        let mut deck = create_deck();
+        shuffle_deck(&mut deck);
 
-  let mut n: u32 = 1;
-  'search:loop {
+        let mut hands: Vec<Vec<&Card>> = vec![];
+        let mut hand: Vec<&Card> = vec![];
 
-    n += 1;
-    let mut deck = create_deck();
-    shuffle_deck(&mut deck);
-  
-    let mut hands: Vec<Vec<&Card>> = vec![];
-    let mut hand: Vec<&Card> = vec![];
-  
-    for (i, card) in deck.iter().enumerate() {
-      hand.push(card);
-  
-      if (i + 1) % 5 == 0 {
-        hands.push(hand.clone());
-        hand.clear();
-      }
+        for (i, card) in deck.iter().enumerate() {
+            hand.push(card);
+
+            if (i + 1) % 5 == 0 {
+                hands.push(hand.clone());
+                hand.clear();
+            }
+        }
+
+        for mut hand in hands {
+            if royal_flush(&mut hand) {
+                println!("Scala Reale! It took exactly {n} decks.");
+                break 'search;
+            }
+        }
     }
-
-    for mut hand in hands {
-      if royal_flush(&mut hand) {
-        println!("Scala Reale! It took exactly {n} decks.");
-        break 'search
-      }
-    }
-  }
 }
